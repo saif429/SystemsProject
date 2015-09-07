c655721820c36064d9f466bdfd4a0e06741f9801
saif429 <saif429@gmail.com>
2015-09-07 18:06:22 -0400

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -64,7 +64,7 @@ char *GetNextToken( Token * tk ) {
   return NULL;
 }
 
-void populateTokenList(char* input)
+void populateTokenList(char* input, int i)
 {
 	
 /*
@@ -125,22 +125,10 @@ int main(int argc, char **argv) {
 	
 	int i = strlen(argv[1]);
 	char arrayofstrings[i];
-	strcpy(arrayofstrings, argv[1]); // copies the string into the array
-	// keep this part, it should be fine, just stores the string into the array using its string length. 
-	// the part below is not right but it splits up the array based off of tabs and whitespaces. 
-	// I kind of understand the If Statements now, we can just check each character then
-	// I'll add the if statements below
+	strcpy(arrayofstrings, argv[1]);
+	populateTokenList(arrayofstrings, i);
 	
-	int g;
-	
-	for (g = 0; g < i; g ++){ // this will go through each index of the array checking each character
-	// the if statements will be in the loop
 
-		if(isspace(arrayofstrings[g])){
-      			printf("This is a white space: %s\n", argv[g] );
-      			printf("This is the index id: %d\n", g);
-      		}
-      	}
 	
   return 0;
 }
